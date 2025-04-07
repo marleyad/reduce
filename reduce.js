f96517dcf9d154448918d4e413c86db2a83eb028
Adam Marley <141704661+marleyad@users.noreply.github.com>
2025-04-07 11:31:38 -0400

    Update reduce.js

--- reduce.js
+++ reduce.js
@@ -1,6 +1,6 @@
 /*
 Write a function called extractValue which accepts an array of objects and a key and returns a new array with the value of each object at the key.
-
+ 
 Examples:
     const arr = [{name: 'Elie'}, {name: 'Tim'}, {name: 'Matt'}, {name: 'Colt'}]
     extractValue(arr,'name') // ['Elie', 'Tim', 'Matt', 'Colt']
